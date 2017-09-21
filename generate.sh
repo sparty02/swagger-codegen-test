7b855d09e8c1d3782747d18f608d9be06f723b53
Ryan Dunckel <Ryan.Dunckel@fisglobal.com>
2017-09-21 18:49:45 -0500

    wip

--- generate.sh
+++ generate.sh
@@ -6,5 +6,9 @@ docker run --rm -v "${PWD}":/local swaggerapi/swagger-codegen-cli generate \
     -i http://petstore.swagger.io/v2/swagger.json \
     -l typescript-angular \
     -c /local/codegen-config.json \
-    -o /local/output/petstore-angular
-    # -t /local/templates \
+    -o /local/output/petstore-angular \
+    -t /local/templates && \
+
+cd output/petstore-angular && \
+npm install || true && \
+cd ../../ || return
